6c321d76f9c3ae80c5992ffdeb30f5a8abe9f941
starambler <duyouman@gmail.com>
2022-03-11 14:59:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,14 +15,14 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 git clone -b master  https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 #svn co --force https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome && svn revert -R package/luci-app-adguardhome
 
-for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
-  svn checkout "https://github.com/immortalwrt/packages/trunk/net/$i" "package/helloworld/$i"; \
-done
-
 #passwall-package 
 #git clone -b packages  https://github.com/xiaorouji/openwrt-passwall.git  package/passwall
 #git clone -b main  https://github.com/starambler/openwrt-passwall.git  package/passwall
 git clone -b master  https://github.com/fw876/helloworld.git  package/helloworld
+
+for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
+  svn checkout "https://github.com/immortalwrt/packages/trunk/net/$i" "package/helloworld/$i"; \
+done
 
 #add upx
 mkdir -p tools/ucl && wget -P tools/ucl https://raw.githubusercontent.com/coolsnowwolf/lede/master/tools/ucl/Makefile 
