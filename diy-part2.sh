b9c9a22bf9def000875f35dfcf1bdd98249d6f6a
starambler <duyouman@gmail.com>
2022-03-15 21:23:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,7 +20,7 @@ git clone  https://github.com/xiaorouji/openwrt-passwall2.git  package/passwall2
 #git clone  https://github.com/xiaorouji/openwrt-passwall.git  package/passwall
 
 git clone -b master  https://github.com/fw876/helloworld.git  package/helloworld
-for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
+for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "ssocks" "redsocks2"; do \
   svn checkout "https://github.com/immortalwrt/packages/trunk/net/$i" "package/helloworld/$i"; \
 done
 #svn checkout https://github.com/immortalwrt/packages/trunk/net/redsocks2    package/helloworld/redsocks2
